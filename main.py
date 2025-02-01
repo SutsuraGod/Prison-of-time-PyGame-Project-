56b72fed7713acf13e53ae32d723e256d45ea19b
Sutsura <sutsura0123@gmail.com>
2025-02-01 16:42:37 +0300

    Добавил счетчик очков

--- main.py
+++ main.py
@@ -12,11 +12,30 @@ from groups import spell_sprites, doors
 from board import generate_level
 from button import Button
 from sound_manager import play_sound, stop_sound, is_sound_playing
+import csv
+
+
+def save_score():
+    global max_score
+    max_score = max([max_score, last_score])
+
+    with open('score.csv', 'r', encoding='utf-8') as file:
+        reader = list(csv.reader(file, delimiter=';', quotechar="'"))
+    
+    if reader:
+        reader[-1] = [last_score, max_score]
+    
+    with open('score.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file, delimiter=';', quotechar="'")
+        writer.writerows(reader)
 
 
 def default_start():
     '''Полное очищение всего для начала новой игры'''
     configs.player = None
+    configs.white_score = 0
+    configs.score = 0
+    configs.time = 0
     groups.all_sprites.empty()
     groups.player_sprites.empty()
     groups.bow_sprites.empty()
@@ -42,6 +61,18 @@ def print_hp(screen, player):
     screen.blit(hp_image, (70, 50))
 
 
+def print_time_and_score(screen):
+    '''Процесс отрисовки таймера и счета'''
+    font = pygame.font.Font(None, 36)
+    score_surface = font.render(f'Current Score: {int(configs.score)}', True, (0, 0, 0))
+    minutes = int(configs.time // 60)
+    seconds = int(configs.time % 60)
+    time_surface = font.render(f'Time: {minutes}:{seconds}', True, (0, 0, 0))
+
+    screen.blit(score_surface, (400, 50))
+    screen.blit(time_surface, (450, 80))
+
+
 def terminate():
     '''Прекращение работы приложения'''
     pygame.quit()
@@ -57,6 +88,11 @@ def main_menu():
 
         menu_text = pygame.font.Font(None, 72).render('MAIN MENU', True, 'White')
         menu_rect = menu_text.get_rect(center=(500, 140))
+        
+        max_score_text = pygame.font.Font(None, 52).render(f'Maximum Score:{max_score}', True, 'White')
+        max_score_rect = max_score_text.get_rect(center=(500, 200))
+        last_score_text = pygame.font.Font(None, 52).render(f'Last Score:{last_score}', True, 'White')
+        last_score_rect = last_score_text.get_rect(center=(500, 240))
 
         play_button = Button(pos=(500, 420), text_input='PLAY', font=pygame.font.Font(None, 72),
                             base_color='White', alt_color='Green')
@@ -64,6 +100,8 @@ def main_menu():
                             base_color='White', alt_color='Green')
         
         screen.blit(menu_text, menu_rect)
+        screen.blit(last_score_text, last_score_rect)
+        screen.blit(max_score_text, max_score_rect)
         
         for button in [play_button, quit_button]:
             button.change_color(mouse_pos)
@@ -141,6 +179,7 @@ def pause_menu():
 
 def play():
     '''Код игрового процесса'''
+    global last_score
     play_sound('in game')
     default_start()
     generate_level(screen, all_sprites)
@@ -208,15 +247,18 @@ def play():
             
         for i in range(last_len := len(groups.current_level.enemies)):
                 enemy = groups.current_level.enemies[i]
+                kill_score = 10
                 if 11 > groups.levels.index(groups.current_level) > 5:
                     enemy_type = 2
                 elif groups.levels.index(groups.current_level) == 11:
                     enemy_type = 3
+                    kill_score = 25
                 else:
                     enemy_type = 1
 
                 enemy.move(configs.player, configs.player.rect, enemy_type, groups.current_level.objects, configs.player.rect.center)
                 if last_len != len(groups.current_level.enemies):
+                    configs.score += kill_score
                     break
                 groups.current_level.enemies[i] = enemy
 
@@ -228,16 +270,17 @@ def play():
         if groups.levels.index(groups.current_level) % 5 == 0:
             for i in groups.current_level.chests:
                 i.update(configs.player.rect.center, keys, screen)
-        
-        clock.tick(configs.FPS)
-        pygame.display.flip()
 
         if configs.player.health <= 0:
+            last_score = int(configs.score)
+            save_score()
             stop_sound('in game')
             stop_sound('boss')
             main_menu()
 
         if not configs.player.playing:
+            last_score = int(configs.score)
+            save_score()
             game_over()
 
         if groups.levels.index(groups.current_level) == 11:
@@ -250,6 +293,13 @@ def play():
                 if not is_sound_playing('in game'):
                     play_sound('in game')
 
+        configs.time += 1 / configs.FPS
+        if configs.score > 0:
+            configs.score -= 1 / configs.FPS
+        print_time_and_score(screen)
+        
+        clock.tick(configs.FPS)
+        pygame.display.flip()
 
 
 def game_over():
@@ -263,6 +313,8 @@ def game_over():
         game_over_rect = game_over_text.get_rect(center=(500, 140))
         you_win_text = pygame.font.Font(None, 72).render('You win!', True, 'White')
         you_win_rect = you_win_text.get_rect(center=(500, 200))
+        run_score_text = pygame.font.Font(None, 52).render(f'Run Score:{last_score}', True, 'White')
+        run_score_rect = run_score_text.get_rect(center=(500, 250))
 
         main_menu_button = Button(pos=(500, 420), text_input='MAIN MENU', font=pygame.font.Font(None, 72),
                             base_color='White', alt_color='Green')
@@ -271,6 +323,7 @@ def game_over():
         
         screen.blit(game_over_text, game_over_rect)
         screen.blit(you_win_text, you_win_rect)
+        screen.blit(run_score_text, run_score_rect)
         
         for button in [main_menu_button, quit_button]:
             button.change_color(mouse_pos)
@@ -301,4 +354,10 @@ if __name__ == "__main__":
     pygame.display.set_caption('Prison of Time')
     clock = pygame.time.Clock()
     ATTACK_EVENT = pygame.USEREVENT + 1
+    with open('score.csv', 'r', encoding='utf-8') as file:
+        reader = csv.reader(file, delimiter=';', quotechar="'")
+        for i, num in enumerate(reader):
+            if i == 1:
+                last_score = int(num[0])
+                max_score = int(num[1])
     main_menu()
